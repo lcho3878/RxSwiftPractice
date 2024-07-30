0ec9742dc75cce746934e61aef8715ea5b2db4ad
lcho3878 <leech3878@naver.com>
2024-07-30 20:58:04 +0900

    SimpleTableView 구현

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -16,11 +16,13 @@ class ViewController: UIViewController {
     
     private let simplePickerView = UIPickerView()
     private let simpleLabel = UILabel()
+    private let simpleTableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
         setupPickerView()
+        setupTableView()
     }
     
     private func configureView() {
@@ -28,16 +30,23 @@ class ViewController: UIViewController {
         
         view.addSubview(simplePickerView)
         view.addSubview(simpleLabel)
+        view.addSubview(simpleTableView)
         
         simplePickerView.snp.makeConstraints {
             $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(100)
         }
+        
         simpleLabel.snp.makeConstraints {
             $0.top.equalTo(simplePickerView.snp.bottom).offset(8)
             $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(44)
-            
+        }
+        
+        simpleTableView.snp.makeConstraints {
+            $0.top.equalTo(simpleLabel.snp.bottom).offset(8)
+            $0.horizontalEdges.equalTo(view.safeAreaInsets)
+            $0.height.equalTo(100)
         }
     }
     
@@ -55,6 +64,28 @@ class ViewController: UIViewController {
         
         simplePickerView.rx.modelSelected(String.self)
             .map { $0.description }
+            .bind(to: simpleLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    private func setupTableView() {
+        simpleTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        let items = Observable.just([
+            "TableView Item1",
+            "TableView Item2",
+            "TableView Item3",
+            "TableView Item4"
+        ])
+        items
+            .bind(to: simpleTableView.rx.items) { tableView, row, element in
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+                cell.textLabel?.text = element
+                return cell
+            }
+            .disposed(by: disposeBag)
+        
+        simpleTableView.rx.modelSelected(String.self)
+            .map { "\($0)을 클릭했습니다." }
             .bind(to: simpleLabel.rx.text)
             .disposed(by: disposeBag)
     }

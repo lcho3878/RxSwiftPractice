582431e395a9fe68380a607d58193bb546527b3f
lcho3878 <leech3878@naver.com>
2024-07-31 01:07:46 +0900

    attributedPickerView 구현

--- RxSwiftPractice/SimplePickerViewController.swift
+++ RxSwiftPractice/SimplePickerViewController.swift
@@ -15,19 +15,28 @@ final class SimplePickerViewController: UIViewController {
     private let disposeBag = DisposeBag()
     
     private let blackPickerView = UIPickerView()
+    private let attributedPickerView = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
         setupBlackPickerView()
+        setupattributedPickerView()
     }
     
     private func configureView() {
         view.backgroundColor = .white
         
         view.addSubview(blackPickerView)
+        view.addSubview(attributedPickerView)
+        
         blackPickerView.snp.makeConstraints {
             $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        attributedPickerView.snp.makeConstraints {
+            $0.top.equalTo(blackPickerView.snp.bottom)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
         }
     }
     
@@ -43,6 +52,16 @@ final class SimplePickerViewController: UIViewController {
             .subscribe(onNext: { value in
                 print("blackPickerView Selected \(value)")
             })
+            .disposed(by: disposeBag)
+    }
+    
+    private func setupattributedPickerView() {
+        let items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        Observable.just(items)
+            .bind(to: attributedPickerView.rx.itemAttributedTitles) { row, element in
+                return NSAttributedString(string: "\(element)",
+                                          attributes: [.foregroundColor: UIColor.systemCyan, .underlineStyle: NSUnderlineStyle.single.rawValue])
+            }
             .disposed(by: disposeBag)
     }
 }
